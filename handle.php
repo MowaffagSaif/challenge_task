ba4aa156cf7a61a6d0b0b1d7907c8a7e5c34be37
MowaffagSaif <107454070+MowaffagSaif@users.noreply.github.com>
2022-11-02 20:00:51 +0200

    Update handle.php

--- handle.php
+++ handle.php
@@ -1,22 +1,23 @@
 <?php
-require 'source.php'; 
+require 'source.php';
+require_once 'db.php' ;
 
 if(isset($_POST['check'])){
     $ip = $_POST['ip'] ;
     $url = "http://services.codesoft.sd/iplocation?ip=".$ip ;
-    $dataOfIp = handleIp($ip, $url);
-    $myArray = json_decode(($dataOfIp), true);
+    
+    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+        $dataOfIp = handleIp($ip, $url); 
+        $myArray = json_decode(($dataOfIp), true);
 
-    echo "Country name: ".$myArray["countryname"]. "<br>";
-    echo "Country Code: ".$myArray["countrycode2"]. "<br>";
-    echo "IP Entered IS : " . $ip . "<br>" ;
+        $code = $myArray["countrycode2"];
+        $url2 = "http://services.codesoft.sd/time?q=".$code ;
+        $dataOfCode = handleIp($code, $url2);
+        $myArray2 = json_decode(($dataOfCode), true);
 
-    $code = $myArray["countrycode2"];
-    $url2 = "http://services.codesoft.sd/time?q=".$code ;
-    $dataOfCode = handleIp($code, $url2);
+        printResult($ip,$myArray , $myArray2);
+    } else {
+        echo("$ip is not a valid IPv4 address");
+}
 
-    $myArray2 = json_decode(($dataOfCode), true);
-
-    echo "Zone name:".$myArray2["zonename"]. "<br>";
-    echo "Time:".$myArray2["time"];
 }
