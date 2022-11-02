6c6ade901e4973e3b7e52522bf924da77cdd2768
MowaffagSaif <107454070+MowaffagSaif@users.noreply.github.com>
2022-11-02 09:09:50 +0200

    Update source.php

--- source.php
+++ source.php
@@ -2,7 +2,7 @@
 
 function handleIp($ip,$url){
     $cURLConnection = curl_init($url);
-    curl_setopt($cURLConnection , CURLOPT_HTTPHEADER, array("REMOTE_ADDR: $ip","HTTP_X_FORWARDED_FOR: $ip"));
+    // curl_setopt($cURLConnection , CURLOPT_HTTPHEADER, array("REMOTE_ADDR: $ip","HTTP_X_FORWARDED_FOR: $ip"));
     curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($cURLConnection);
     curl_close($cURLConnection);
@@ -10,10 +10,3 @@ function handleIp($ip,$url){
     return $result ;
 }
 
-
-// function handleCode($code,$url){
-//     $cURLConnection = curl_init($url);
-//     curl_setopt($cURLConnection , CURLOPT_HTTPHEADER, array("REMOTE_ADDR: $code","HTTP_X_FORWARDED_FOR: $code"));
-//     $result = curl_exec($cURLConnection);
-//     curl_close($cURLConnection);
-// }
